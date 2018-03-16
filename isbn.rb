e9f436118c1a4ce3b63d0658407ab4f6e989e79e
Aaron Gooden <aagooden@me.com>
2018-03-15 20:17:36 -0400

    Added validate_length function and tests passing

--- isbn.rb
+++ isbn.rb
@@ -2,7 +2,18 @@ def prepare(num)
 
 #preparing the number by getting rid of spaces
 	num.delete!(" ")
-	
+
 	return num
+
+end
+
+def validate_length(num)
+	valid = true
+
+	if num.length != 10
+		valid = false
+	end
+
+valid
 
 end
--- test_isbn.rb
+++ test_isbn.rb
@@ -17,9 +17,21 @@ class Isbn_test < Minitest::Test
 		assert_equal("234x", prepare(num))
 	end
 
+	def test_check_if_number_is_not_10_digits_long_false_if_not
+		num = "1234567890x"
+		assert_equal(false, validate_length(num))
+	end
 
+	def test_check_if_number_is_not_10_digits_long_false_if_not_short_num
+		num = "1234560x"
+		assert_equal(false, validate_length(num))
+	end
 
-
+	def test_prepare_and_validate_functions_working_together_properly
+		num = " 12 345 67890 x "
+		prepared = prepare(num)
+		assert_equal(false, validate_length(prepared))
+	end
 
 end
 
