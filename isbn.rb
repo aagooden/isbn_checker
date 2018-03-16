e05500d176a5daa73b70e563b955531a23e3d7a8
Aaron Gooden <aagooden@me.com>
2018-03-15 21:03:22 -0400

    Added final validate_number calculation and returning true if valid and false if invalid.  All original tests passing

--- isbn.rb
+++ isbn.rb
@@ -23,7 +23,35 @@ end
 def validate_number(num)
 	valid = true
 
-	return valid
+#setting my check sum here... if it is x or X check sum is 10 else check is converted to
+	check = num[num.length - 1]
+	if check == "x" || check =="X"
+		check == 10
+	else
+		check = check.to_i
+	end
+
+#performing validation math
+
+# counter will represent the num's "position (1-length) which
+# will be multiplied by char which is the actual integer value of the num
+# num.length - 2 is used to account for the index shift AND the fact that the last digit in num is the check sum
+	counter = 1
+	sum = 0
+	for x in (0..num.length - 2) do
+		char = num[x].to_i
+		sum = sum + (char * counter)
+		counter +=1 
+	end
+
+#final comparison for validation
+	sum = sum % 11
+	if sum == check
+		return valid
+	else
+		return false
+	end
+
 end
 
 
